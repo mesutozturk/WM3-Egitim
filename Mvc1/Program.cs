134cdc36f825a6dab12be0b70c4bf625b81a7ce0
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2022-03-16 14:12:32 +0300

    .

--- Mvc1/Program.cs
+++ Mvc1/Program.cs
@@ -1,6 +1,25 @@
+//Configure services
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+builder.Services.AddControllersWithViews();
+// builder.Services.AddRazorPages();
+// builder.Services.AddServerSideBlazor();
+
+//Configure - Middleware
+app.UseHttpsRedirection();
+app.UseStaticFiles(); //wwwroot içindeki statik (html-css-js-img-video-audio)
+//app.UseCookiePolicy();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+//app.UseSession();
+//app.MapRazorPages();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
